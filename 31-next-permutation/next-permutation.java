94d189057a97028a1cca60828e10863271faf2b2
LINTHAN B L <linthanblpoojari21@gmail.com>
2026-03-08 12:46:06 +0530

    Time: 2 ms (8.69%) | Memory: 44.7 MB (67.63%) - LeetSync

---
+++ 31-next-permutation/next-permutation.java
@@ -0,0 +1,36 @@
+class Solution {
+    public void nextPermutation(int[] nums) {
+        int idx1 = -1;
+        int idx2 = -1;
+
+        for(int i = nums.length - 2; i>=0; i--){
+            if(nums[i] < nums[i+1]){
+                idx1 = i;
+                break;
+            }
+        }
+
+        if(idx1 < 0){
+            Arrays.sort(nums);
+            return;
+        }
+        else{
+            for(int i = nums.length - 1; i>=0; i--){
+                if(nums[i] > nums[idx1]){
+                    idx2 = i;
+                    break;
+                }
+            }
+        }
+
+        swap(nums, idx1, idx2);
+
+        Arrays.sort(nums, idx1 + 1, nums.length);
+    }
+
+        private  void swap(int[] nums, int i, int j){
+            int temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
+        }
+}
