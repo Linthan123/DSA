faf5c3160f46b77f154df91e2c723b65e5e7a9b2
LINTHAN B L <linthanblpoojari21@gmail.com>
2026-03-08 15:17:44 +0530

    Time: 2 ms (68.43%) | Memory: 44.8 MB (53.80%) - LeetSync

---
+++ 22-generate-parentheses/generate-parentheses.java
@@ -0,0 +1,26 @@
+class Solution {
+    public List<String> generateParenthesis(int n) {
+        int oc = 0;
+        int cc = 0;
+        List<String> result = new ArrayList<>();
+
+        solve(n, result, oc, cc, "");
+        return result;
+    }
+        private void solve(int n, List<String> result, int oc, int cc, String s){
+
+            if(oc == n && cc == n){
+           result.add(new String(s));
+           return;
+        }
+
+        if(oc < n){
+            solve(n, result, oc + 1, cc, s+"(");
+        }
+
+        if(cc < oc){
+            solve(n, result, oc, cc + 1, s+")");
+        }
+        }
+        
+}
